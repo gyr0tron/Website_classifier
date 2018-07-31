0df2d6c639f8f5a0aa96178045a602b1e1df0ab4
Gyrotron <simmarkalsi@gmail.com>
2018-08-01 01:27:12 +0530

    idk man i was just trying things

--- classifier/website_category_classifier.py
+++ classifier/website_category_classifier.py
@@ -95,8 +95,8 @@ def success(name):
     # Define model and setup tensorboard
     model = tflearn.DNN(net, tensorboard_dir='tflearn_logs')
 
-    model.fit(train_x, train_y, n_epoch=1000, batch_size=8, show_metric=True)
-    model.save('model.tflearn')
+    # model.fit(train_x, train_y, n_epoch=1000, batch_size=8, show_metric=True)
+    # model.save('model.tflearn')
     model.load('model.tflearn')
 
 
